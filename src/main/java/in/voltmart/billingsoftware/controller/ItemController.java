c91e1fdb0a7132f6e4e56266c4a7c12719a1274c
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-09-10 19:32:29 +0530

    Add ItemController with CRUD endpoints for items (create, fetch, delete) using ItemService

--- src/main/java/in/voltmart/billingsoftware/controller/ItemController.java
+++ src/main/java/in/voltmart/billingsoftware/controller/ItemController.java
@@ -1,4 +1,58 @@
 package in.voltmart.billingsoftware.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import in.voltmart.billingsoftware.io.ItemRequest;
+import in.voltmart.billingsoftware.io.ItemResponse;
+import in.voltmart.billingsoftware.service.ItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/item")
 public class ItemController {
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+
+    @PostMapping("/admin/categories")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ItemResponse addItem(@RequestPart("Item") String ItemString,
+                                        @RequestPart("file") MultipartFile file){
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ItemRequest request = null;
+        try {
+            request = objectMapper.readValue(ItemString, ItemRequest.class);
+            return itemService.addItem(request, file);
+        } catch (JsonProcessingException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Exception occurred while parsing the json: "+ex.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    @GetMapping
+    public List<ItemResponse> fetchCategories() {
+        return itemService.fetchAllItems();
+    }
+
+    @DeleteMapping("/admin/categories{ItemId}")
+    public void  remove(@PathVariable String ItemId) {
+        try {
+            itemService.deleteItem(ItemId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,e.getMessage());
+        }
+    }
 }
