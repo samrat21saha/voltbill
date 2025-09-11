2258f90a39d9f23d25ec780a1e74132d7fddfe99
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-09-11 06:22:10 +0530

    Add OrderResponse DTO with order details, items, totals, payment info, and timestamps

--- src/main/java/in/voltmart/billingsoftware/io/OrderResponse.java
+++ src/main/java/in/voltmart/billingsoftware/io/OrderResponse.java
@@ -1,4 +1,38 @@
 package in.voltmart.billingsoftware.io;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class OrderResponse {
+    private String orderId;
+    private String customerName;
+    private String phoneNumber;
+    private List<OrderRequest.OrderItemRequest> items;
+    private Double subtotal;
+    private Double tax;
+    private Double grandTotal;
+    private PaymentMethod paymentMethod;
+    private LocalDateTime createdAt;
+    private PaymentDetails paymentDetails;
+
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class OrderItemRequest {
+        private String itemId;
+        private String name;
+        private Double price;
+        private Integer quantity;
+    }
 }
