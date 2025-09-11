a38a43b38765a1e300040da61a9fa10654bfea0a
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-09-11 06:19:14 +0530

    Add OrderItemEntity with fields for itemId, name, price, and quantity

--- src/main/java/in/voltmart/billingsoftware/entity/OrderItemEntity.java
+++ src/main/java/in/voltmart/billingsoftware/entity/OrderItemEntity.java
@@ -1,4 +1,25 @@
 package in.voltmart.billingsoftware.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_order_items")
 public class OrderItemEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String itemId;
+    private String name;
+    private Double price;
+    private Integer quantity;
 }
