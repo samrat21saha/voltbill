2a0a84bc013a3c678670d331fbce84d52d3f318e
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-09-12 22:06:21 +0530

    feat: introduce PaymentRequest DTO to handle Razorpay payment verification input

--- src/main/java/in/voltmart/billingsoftware/io/PaymentRequest.java
+++ src/main/java/in/voltmart/billingsoftware/io/PaymentRequest.java
@@ -1,4 +1,13 @@
 package in.voltmart.billingsoftware.io;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentRequest {
+    private Double amount;
+    private String currency;
 }
