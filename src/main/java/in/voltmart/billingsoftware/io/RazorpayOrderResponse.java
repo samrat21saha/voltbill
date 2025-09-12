946e8c4a7e48fe831d1a287464877d8046293246
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-09-12 22:07:12 +0530

    feat: add RazorpayOrderResponse DTO for handling order creation response from Razorpay API

--- src/main/java/in/voltmart/billingsoftware/io/RazorpayOrderResponse.java
+++ src/main/java/in/voltmart/billingsoftware/io/RazorpayOrderResponse.java
@@ -1,4 +1,26 @@
 package in.voltmart.billingsoftware.io;
 
+import jdk.jshell.Snippet;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RazorpayOrderResponse {
+
+    private String id;
+    private String entity;
+    private Integer amount;
+    private String currency;
+    private String status;
+    private Date createdAt;
+    private String receipt;
+
+
 }
